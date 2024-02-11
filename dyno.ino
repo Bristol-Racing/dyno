e33dddbfc79b716856ea281e1ec9f9c5726344ce
TBExtent <jedpreist@gmail.com>
2024-02-11 21:58:32 +0000

    commented

--- dyno.ino
+++ dyno.ino
@@ -5,61 +5,70 @@
 #include "sensors/temperature.hpp"
 #include "sensors/sensorManager.hpp"
 
-// HX711 circuit wiring
+// Sensor pins
 const int LOADCELL_DOUT_PIN = 3;
 const int LOADCELL_SCK_PIN = 4;
-
 #define CURRENT_VIN A0
 #define VOLTAGE_VIN A6
 #define TEMPERATURE_VIN A3
+//  Plus pin 2 for hall effect
 
-const int time_per_reading = 100;
-const int readings = 10;
+//  tick every 100ms
+const int time_per_tick = 100;
+//  callback every 100ms
+const int time_per_callback = 1000;
 
+//  Load cell arm length
 const double arm_length = 142.5 / 1000.0;  //  meters
 
-int currentReadings = 0;
-long reading = 0;
-
+//  Initialise the 5 sensors
 int sensorCount = 5;
 Sensor::LoadCell scale(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN, arm_length);
 Sensor::HallEffect hallEffect;
-Sensor::CurrentSensor currentSensor;
-Sensor::VoltageSensor voltageSensor(VOLTAGE_VIN, 993000.0, 219200.0);
+Sensor::CurrentSensor currentSensor(1.0, 1.0);  //  Not calibrated
+Sensor::VoltageSensor voltageSensor(VOLTAGE_VIN, 1.0);  //  Not calibrated
 Sensor::TemperatureSensor temperature(TEMPERATURE_VIN);
 
-Sensor::SensorManager manager(sensorCount, time_per_reading * readings);
+//  Initialise the manager
+Sensor::SensorManager manager(sensorCount, time_per_callback);
 
 void setup() {
+    //  Start serial communication
     Serial.begin(57600);
 
-    scale.setTickRate(time_per_reading);
-    scale.setReadRate(1000);
+    //  set tick and report rate for the load cell
+    scale.setTickRate(time_per_tick);
+    scale.setReportRate(1000);
 
+    //  Setup the current sensor and set tick and report rate
     currentSensor.setup();
-    currentSensor.setTickRate(time_per_reading);
-    currentSensor.setReadRate(1000);
+    currentSensor.setTickRate(time_per_tick);
+    currentSensor.setReportRate(1000);
 
-    hallEffect.setTickRate(1);
-    hallEffect.setReadRate(1000);
+    //  Other sensors just need report rates. All 1 second
+    hallEffect.setReportRate(1000);
+    voltageSensor.setReportRate(1000);
+    temperature.setReportRate(1000);
 
-    voltageSensor.setReadRate(1000);
-
-    temperature.setReadRate(1000);
-
+    //  Add all the sensors to the manager
     manager.addSensor(&scale);
     manager.addSensor(&hallEffect);
     manager.addSensor(&currentSensor);
     manager.addSensor(&voltageSensor);
     manager.addSensor(&temperature);
-    manager.setReadCallback(&readCallback);
+
+    //  Set the callback function
+    manager.setReportCallback(&reportCallback);
 }
 
 void loop() {
+    //  Spin the manager, so that it schedules and handles sensor readings
     manager.spin();
 }
 
-void readCallback(double * results) {
+//  Called by the sensor manager to pass readings back to the main program
+void reportCallback(double * results) {
+    //  Goes through and prints out all the sensor readings, separated by commas
     for (int i = 0; i < sensorCount; i++) {
         if (i > 0) {
             Serial.print(",");
